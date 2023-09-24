a7e34dec6aef25cd93d590b1f0166ce02cc4f0ee
ValentineYoungMan <90317015+ValentineYoungMan@users.noreply.github.com>
2023-09-24 15:50:22 +0300

    Update recipesSlice.ts

--- src/redux/reducers/recipesSlice.ts
+++ src/redux/reducers/recipesSlice.ts
@@ -17,9 +17,6 @@ export type setFilter = {
 };
 
 const initialState: RecipeSliceState = {
-  //   recipes,
-  //   newRecipe,
-  //   filteredCategoryRecipe: [],
   categoryValue: 'All',
   isLiked: false,
 };
@@ -28,42 +25,9 @@ const recipesSlice = createSlice({
   name: 'recipesSlice',
   initialState,
   reducers: {
-    // addRecipe(state, action: PayloadAction<RecipeItem>) {
-    //   state.recipes = [...state.recipes, action.payload];
-    //   //console.log(state.recipes);
-    // },
-    // changeName(state, action: PayloadAction<string>) {
-    //   state.newRecipe.name = action.payload;
-    // },
-    // addImg(state, action: PayloadAction<string>) {
-    //   state.newRecipe.imageUrl = action.payload;
-    // },
-    // addDescription(state, action: PayloadAction<string>) {
-    //   state.newRecipe.description = action.payload;
-    // },
-    // addInstruction(state, action: PayloadAction<string>) {
-    //   state.newRecipe.instruction = action.payload;
-    // },
-    // setCategory(state, action: PayloadAction<number>) {
-    //   state.newRecipe.category = action.payload;
-    // },
-    // addIngredient(state, action: PayloadAction<string>) {
-    //   state.newRecipe.ingredients = [...state.newRecipe.ingredients, action.payload];
-    // },
-    // removeIngredient(state, action: PayloadAction<string>) {
-    //   state.newRecipe.ingredients = state.newRecipe.ingredients.filter(
-    //     (item) => item != action.payload,
-    //   );
-    // },
     changeIsLiked(state, action: PayloadAction<boolean>) {
       state.isLiked = action.payload;
     },
-    // deleteRecipe(state, action: PayloadAction<RecipeItem[]>) {
-    //   state.recipes = action.payload;
-    // },
-    // chooseCategory(state, action: PayloadAction<RecipeItem[]>) {
-    //   state.recipes = action.payload;
-    // },
     setCategoryId(state, action: PayloadAction<string>) {
       state.categoryValue = action.payload;
     },
@@ -73,16 +37,7 @@ const recipesSlice = createSlice({
 export const selectRecipesSliceItems = (state: RootState) => state.recipesSlice;
 
 export const {
-  //   addRecipe,
-  //   changeName,
-  //   addImg,
-  //   addDescription,
-  //   addInstruction,
-  //   setCategory,
-  //   addIngredient,
-  //   removeIngredient,
   changeIsLiked,
-  //   deleteRecipe,
   setCategoryId,
 } = recipesSlice.actions;
 
